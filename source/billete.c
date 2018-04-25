466e1a6b8607d9c0073320540109e812c9895d7b
FerMod <tudelaigarcia@gmail.com>
2018-04-25 12:24:58 +0200

    Add 'billete' files

---
+++ source/billete.c
@@ -0,0 +1,12 @@
+
+/*-------------------------------------
+ screenText.c
+-------------------------------------*/
+
+#include <nds.h>
+#include <stdio.h>
+
+
+void SpawnBillete() {
+	MostrarBillete(0, rand()%255, 0);
+}
