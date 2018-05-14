b46bdb76c470f3c327e24b5f65cc101283a644b1
FerMod <tudelaigarcia@gmail.com>
2018-05-14 16:48:58 +0200

    Update binary files

--- build/Fondo.h
+++ build/Fondo.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 98304 = 98304
 //
-//	Time-stamp: 2018-05-14, 10:19:51
+//	Time-stamp: 2018-05-14, 10:25:41
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
